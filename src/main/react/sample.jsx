eb755254fad85558b206d5ce29624065f5fcc88e
Jorge Morla <jorgemorlapro@gmail.com>
2024-03-29 18:49:26 -0400

    sample page

--- src/main/react/sample.jsx
+++ src/main/react/sample.jsx
@@ -6,9 +6,10 @@ function SamplePage() {
     return (
         <div class="container mt-5">
             <div class="jumbotron">
-                <h1 class="display-4">Welcome to Our Website!</h1>
+                <h1 class="display-4">Welcome to this page!</h1>
                 <p class="lead">This is a simple hero unit, a simple Page component for calling extra attention to featured content or information.</p>
                 <hr class="my-4" />
+                <small class="text-muted">Powered by jmorla</small>
             </div>
         </div>
     )
